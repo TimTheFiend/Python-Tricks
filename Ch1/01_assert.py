e6edb671686ecc8513aded8e44070df0c71cd96d
Joakim <33222649+TimTheFiend@users.noreply.github.com>
2019-10-28 08:14:22 +0100

    Assert statements

--- Ch1/01_assert.py
+++ Ch1/01_assert.py
@@ -2,6 +2,16 @@ def apply_discount(product, discount):
     price = int(product['price'] * (1.0 - discount))
     """Assert:
                 If this isn't true, throw an exception
+
+        Assertions are meant to be internal self-checks for you program.
+        They work by declaring some conditions as impossible in your code.
+        If one of these conditions doesn't hold, that means there's a bug in the program.
+
+        If your program is bug-free, these conditions will never occur.
+        But if they do occur, the program will crash with an assertion error telling you exactly which
+        "impossible" condition was triggered. This makes it much easier to track down and fix bugs in your program.
+
+        Keep in mind that assert statements is a debugging aid, not a mechanism for handling run-time errors.
     """
     try:
         assert 0 <= price <= product['price']
