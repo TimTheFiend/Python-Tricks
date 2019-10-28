7d0894573d3f44473985028277befbb5def00b85
Joakim <33222649+TimTheFiend@users.noreply.github.com>
2019-10-28 08:46:11 +0100

    more context managers

--- 2.3 context managers.py
+++ 2.3 context managers.py
@@ -7,6 +7,11 @@ def managed_file(name):
     """For example, you can use the contextlib.contextmanager decorator to define a generator-based
         factory function for a resource that will then automatically support the with statement.
         Here's what rewriting our ManagedFile context manager exampel with this technique looks like:
+
+        In this case, managed_file() is a generator that first acquires the resource.
+        After that, it temporarily suspends its own execution and yields the resource so it can be used by the caller.
+        When the caller leaves the with context, the generator continues to execute so that any remaining
+        clean-up steps can occur and the resource can get released back to the system.
     """
     try:
         f = open(name, "w")
@@ -33,10 +38,42 @@ class ManagedFile:
         if self.file:
             self.file.close()
 
-# with ManagedFile('hello.txt') as f:
-#     f.write('hello world!')
-#     f.write('bye')
+class Indenter:
+    def __init__(self):
+        self.level = 0
 
-with managed_file('hello.txt') as f:
-    f.write('hello world!')
-    f.write('bye')
+    def __enter__(self):
+        self.level += 1
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.level -= 1
+
+    def print(self, text):
+        print('    ' * self.level + text)
+
+def indenter_as_context_manager():
+    with Indenter() as indent:
+        indent.print("hi")
+        with indent:
+            indent.print("howdy")
+            with indent:
+                indent.print("Yeehaw")
+        indent.print("every modern american president is a war criminal.")
+
+
+#region VALG AF METHOD AT KØRER
+what_function_to_run = int(input("Hvilken function vil du kører? "))
+if what_function_to_run == 1:
+    with ManagedFile('hello.txt') as f:
+        f.write('hello world!')
+        f.write('bye')
+elif what_function_to_run == 2:
+    with managed_file('hello.txt') as f:
+        f.write('hello world!')
+        f.write('bye')
+elif what_function_to_run == 3:
+    indenter_as_context_manager()
+else:
+    print("Ya didn't change it")
+#endregion
