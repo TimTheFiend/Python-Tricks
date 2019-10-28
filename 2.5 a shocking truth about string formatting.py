2580453539387da1fe6c76419e1759304bb29ac6
Joakim <33222649+TimTheFiend@users.noreply.github.com>
2019-10-28 09:30:53 +0100

    string formatting

---
+++ 2.5 a shocking truth about string formatting.py
@@ -0,0 +1,58 @@
+def old_style_string_formatting():
+    print("Hello %s" % "billybob")
+
+def old_style_with_tuple():
+    print("Hello %s, you %s" % ("Billybob", "sex offender"))
+
+def old_style_dictionary():
+    print("Hello %(name)s, you %(jobdescription)s." % {"name": "billybob", "jobdescription": "sex offender"})
+
+def new_style_string_formatting():
+    print("Hello {}".format("billybob"))
+
+def new_style_string_formatting_multiple():
+    print("hey {name}, you {job}".format(name="Biblybob", job="catholic priest"))
+
+def literal_string_interpolation_python_three_six_plus():
+    name = "recently released from jail, Biblebob."
+    print(f"Hello {name}")
+
+def literal_string_maths():
+    a, b = 5, 10
+    print(f"five plus ten is {a + b} and not {2 * (a + b)}")
+
+def literal_strings_as_arguments(name, question):
+    print(f"Alright {name}, {question}")
+
+try:
+    select_function = int(input("Vælg en funktion: "))
+except ValueError:
+    exit(0)
+if select_function == 1:
+    old_style_string_formatting()
+elif select_function == 2:
+    old_style_with_tuple()
+elif select_function == 3:
+    old_style_dictionary()
+elif select_function == 4:
+    new_style_string_formatting()
+elif select_function == 5:
+    new_style_string_formatting_multiple()
+elif select_function == 6:
+    literal_string_interpolation_python_three_six_plus()
+elif select_function == 7:
+    literal_string_maths()
+elif select_function == 8:
+    literal_strings_as_arguments("Biblebob", "Where are the bodies buried?")
+elif select_function == 9:
+    pass
+elif select_function == 10:
+    pass
+elif select_function == 11:
+    pass
+elif select_function == 12:
+    pass
+elif select_function == 13:
+    pass
+else:
+    print("guess i'll stop")
