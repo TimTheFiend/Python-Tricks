de545240572326c5fa4f0eee5870efa9277d537a
Joakim <33222649+TimTheFiend@users.noreply.github.com>
2019-10-28 08:21:04 +0100

    commas

---
+++ 02_complacent comma placement.py
@@ -0,0 +1,13 @@
+# Works as a normal string but 
+my_str = ("I thought what I'd do was, "
+          "I'd pretend to be one "
+          "of those deaf-mutes.")
+
+# Setting up tuples, lists, dictionaries, etc. like this makes it easier to
+# see what was added down the, and adding the trailing comma at the end
+# ensures no dumb mistakes will happen if more things are added,
+# and won't cause any errors, so might as well.
+names = [
+    "Holden",
+    "Salinger",
+]
