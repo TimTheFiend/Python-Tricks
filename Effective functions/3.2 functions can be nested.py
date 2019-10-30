1eb32b35f5d7a34f0750b0c72d26cc6443c388fa
Joakim <33222649+TimTheFiend@users.noreply.github.com>
2019-10-30 14:49:46 +0100

    nested functinos

---
+++ Effective functions/3.2 functions can be nested.py
@@ -0,0 +1,28 @@
+def speak(text):
+    def whisper(t):
+        return t.lower() + "..."
+    return whisper(text)
+
+# print(speak("Hello world!"))
+
+"""Returnere en funktion baseret på volume
+
+Returns:
+    function -- yell / whisper
+"""
+def get_speak_func(volume):
+    def whisper(text):
+        return text.lower() + "..."
+    def yell(text):
+        return text.upper() + "!"
+    if volume > 0.5:
+        return yell
+    else:
+        return whisper
+
+get_speak_func(0.3)
+print(get_speak_func(0.3))
+whisper_func = get_speak_func(0.3)
+yell_func = get_speak_func(0.8)
+print(whisper_func("Hello World"))
+print(yell_func("Hello World"))
