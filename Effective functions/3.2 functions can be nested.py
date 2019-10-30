ed87a7b319f2aae85cd60c6166df1ca562a13c56
Joakim <33222649+TimTheFiend@users.noreply.github.com>
2019-10-30 14:58:03 +0100

    this is harem

--- Effective functions/3.2 functions can be nested.py
+++ Effective functions/3.2 functions can be nested.py
@@ -5,6 +5,8 @@ def speak(text):
 
 # print(speak("Hello world!"))
 
+
+#region Nested functions
 """Returnere en funktion baseret på volume
 
 Returns:
@@ -20,9 +22,22 @@ def get_speak_func(volume):
     else:
         return whisper
 
-get_speak_func(0.3)
-print(get_speak_func(0.3))
-whisper_func = get_speak_func(0.3)
-yell_func = get_speak_func(0.8)
-print(whisper_func("Hello World"))
-print(yell_func("Hello World"))
+# # get_speak_func(0.3)
+# # print(get_speak_func(0.3))
+# # whisper_func = get_speak_func(0.3)
+# # yell_func = get_speak_func(0.8)
+# # print(whisper_func("Hello World"))
+# # print(yell_func("Hello World"))
+#endregion
+
+def speak_func(text, volume):
+    def whisper(apples):
+        return text.lower() + "..." + apples
+    def yell():
+        return text.upper() + "!"
+    if volume > 0.5:
+        return yell
+    else:
+        return whisper
+print(speak_func("hello world", 0.3)("helloooooooooo"))
+print(speak_func("hello world", 0.8)())
