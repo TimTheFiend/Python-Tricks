5ad07a9a3117ad1b3905e1acfc4b485e9d114aa8
Joakim <33222649+TimTheFiend@users.noreply.github.com>
2019-11-07 14:40:04 +0100

    data structures finished

--- Ch5 Common data structures/5.1 array data structure.py
+++ Ch5 Common data structures/5.1 array data structure.py
@@ -1,3 +1,28 @@
+#region list - mutable dynamic arrays 
+arr = ['one', 'two', 'three']
+print(arr[0]) # one
+# arrays also have an repr
+print(arr)
+# mutable
+arr[1] = 'ayy'
+del arr[2]
+arr.append(23)
+#endregion
+
+#region tuple - immutable containers
+arr = 'one', 'two', 'three'
+print(arr[0]) # one
+
+# repr
+print(arr)
+
+# immutable
+arr[1] = 'hello' # TypeError
+del arr[2]       # TypeError
+arr.append(23)   # ('one', 'two', 'three', 23)
+#endregion
+
+#region array.array - basic typed arrays
 """Arrays created with the array.array class are mutable and behave like lists.
 except that they are typed arrays, constrained to a single data type.
 """
@@ -11,3 +36,4 @@ try:
     arr[1] = 'oi'
 except TypeError:
     print("must be a number")
+#endregion
--- temp_holding_file.py
+++ temp_holding_file.py
@@ -2,32 +2,3 @@
 #region
 #endregion
 
-#region list - mutable dynamic arrays 
-arr = ['one', 'two', 'three']
-print(arr[0]) # one
-# arrays also have an repr
-print(arr)
-# mutable
-arr[1] = 'ayy'
-del arr[2]
-arr.append(23)
-#endregion
-
-#region tuple - immutable containers
-arr = 'one', 'two', 'three'
-print(arr[0]) # one
-
-# repr
-print(arr)
-
-# immutable
-arr[1] = 'hello' # TypeError
-del arr[2]       # TypeError
-arr.append(23)   # ('one', 'two', 'three', 23)
-#endregion
-
-#region array.array - basic typed arrays
-
-#endregion
-
-
