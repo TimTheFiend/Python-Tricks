8da182a063a2639bcd6eddb865fe1fa99f944867
Joakim <33222649+TimTheFiend@users.noreply.github.com>
2019-11-08 14:10:18 +0100

    pete buttigieg amount of changes very little has changed, but it said it did something

--- Ch4 Classes & OOP/4.2a string conversion or every class needs a dunder repr.py
+++ Ch4 Classes & OOP/4.2a string conversion or every class needs a dunder repr.py
@@ -2,13 +2,13 @@ class Car:
     def __init__(self, color, mileage):
         self.color = color
         self.mileage = mileage
-    
+
     def __str__(self):
-        """Inspecting my_car in an interpreter sessino still gives the <__main__.Car object at 0x00000227F7A39880> output"""
+        """Inspecting my_car in an interpreter session still gives the <__main__.Car object at 0x00000227F7A39880> output"""
         return f'a {self.color} car'
+
 
 my_car = Car('Red', 1312)
 print(my_car)                           # Pre-__str__ Output: <__main__.Car object at 0x000002A8C5FA9880>
 print(my_car.color, my_car.mileage)     # Output: Red 1312
 print(my_car)                           # Post-__str__ Output: a Red car
-
--- Ch6 Writing Pythonic Loops/6.2 comprehending comprehensions.py
+++ Ch6 Writing Pythonic Loops/6.2 comprehending comprehensions.py
@@ -1,5 +1,5 @@
 squares = [x * x for x in range(10)]
-print(squares) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(squares)  # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
 """Pseudo-code for squares = [x * x for x in range(10)]
 values = [expression for item in collection]
@@ -8,7 +8,7 @@ values = [expression for item in collection]
 # If you wanted to make this with a for-loop, it'd probably look like this:
 squares1 = []
 for x in range(10):
-    squares1.append(x * x) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+    squares1.append(x * x)  # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 print(squares1)
 """Pseudo-code for ^
 values = []
@@ -18,7 +18,7 @@ for item in collection:
 
 # list comprehension with condition:
 even_squares = [x * x for x in range(10) if x % 2 == 0]
-print(even_squares) # [0, 4, 16, 36, 64]
+print(even_squares)  # [0, 4, 16, 36, 64]
 """opposite example:
 even_squares = []
 for x in range(10):
@@ -30,13 +30,12 @@ for x in range(10):
 even_squares = [x * x
                 for x in range(10)
                 if x % 2 == 0]
-print(even_squares) # [0, 4, 16, 36, 64]
+print(even_squares)  # [0, 4, 16, 36, 64]
 
 # Set comprehension:
 set_comp = {x * x for x in range(-9, 10)}
-print(set_comp) # {64, 1, 0, 36, 4, 9, 16, 81, 49, 25}
+print(set_comp)  # {64, 1, 0, 36, 4, 9, 16, 81, 49, 25}
 
 # dictionary comprehension:
 dict_comp = {x: x * x for x in range(5)}
-print(dict_comp) # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16}
-
+print(dict_comp)  # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16}
