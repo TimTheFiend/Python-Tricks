bee7d91da9e040cd78619168910d53df52ad5d31
Joakim <33222649+TimTheFiend@users.noreply.github.com>
2019-11-11 11:49:27 +0100

    Sorting dictionaries......

--- Ch7 Dictionary/7.2 sorting dictionaries for fun and profit.py
+++ Ch7 Dictionary/7.2 sorting dictionaries for fun and profit.py
@@ -1,2 +1,25 @@
+"""Sorting
+"""
 xs = {'a': 4, 'c': 2, 'b': 3, 'd': 1}
 print(sorted(xs.items(), key=lambda x: x[1])) # [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+
+"""with operator  
+"""
+import operator
+
+xs = {'a': 4, 'c': 2, 'b': 3, 'd': 1}
+print(sorted(xs.items(), key=operator.itemgetter(1))) # [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+
+"""More Lambda
+"""
+xs = {'a': 4, 'c': 2, 'b': 3, 'd': 1}
+print(sorted(xs.items(), key=lambda x: abs(x[1]))) # [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+
+
+xs = {'a': 4, 'c': 2, 'b': 3, 'd': 1}
+print(sorted(xs.items(),
+            key=lambda x: x[1],
+            reverse=True)) # [('a', 4), ('b', 3), ('c', 2), ('d', 1)]
+
+
+
